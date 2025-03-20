1e1f69c93733aebe5292f56c3c4f4229b3ba1c69
Leighton075 <leighton.simmons075@gmail.com>
2025-03-20 14:03:49 +1300

    added a bunch of stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -111,20 +111,6 @@
   });
 })(jQuery);
 
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-
 document.addEventListener('DOMContentLoaded', () => {
   fetch('/api/benchmarks') // Use relative URL to fetch data from the server
     .then(response => response.json())
