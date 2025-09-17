7dfbabbc888582b763e859eae9212f5f66df01da
Leighton075 <leighton.simmons075@gmail.com>
2025-09-17 15:04:31 +1200

    fix: enable error reporting for improved debugging in booking handler

--- booking_handler.php
+++ booking_handler.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $mysqli = new mysqli("localhost", "skyline_user", "secure_password", "skyline");
 if ($mysqli->connect_errno) {
     http_response_code(500);
