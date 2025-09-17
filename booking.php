295f2e1a82f0152aae06c497cde61d21af3c4e5e
Leighton075 <leighton.simmons075@gmail.com>
2025-09-17 17:01:10 +1200

    feat: update navigation to link directly to the booking page; remove duplicate Book link

--- booking.php
+++ booking.php
@@ -31,13 +31,10 @@
                 <a class="nav-link" href="index.php#features">Attractions</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="events.php">Events</a>
+                <a class="nav-link" href="booking.php">Book</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="gallery.php">Gallery</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="booking.php">Book</a>
             </li>
         </ul>
         <a href="login.php" class="btn login-btn btn-outline-accent my-2 my-sm-0" style="font-size: 10px !important;font-family: poppins !important;">LOGIN</a>
@@ -119,6 +116,9 @@
 $('#returnTopBtn').on('click', function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+</script>
+</body>
+</html>
 </script>
 </body>
 </html>
